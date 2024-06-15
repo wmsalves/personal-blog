e339fd72a46a07061288443c68cfc00930377564
Luke Berry <lukeberrypi@gmail.com>
2024-06-15 02:51:37 +0200

    remove remove remove

--- components/ArticleListItem.tsx
+++ components/ArticleListItem.tsx
@@ -8,13 +8,12 @@ interface Props {
 const ArticleItemList = ({ category, articles }: Props) => {
   return (
     <div className="flex flex-col gap-5">
-      <h2 className="font-cormorantGaramond text-4xl">{category}</h2>
-      <div className="flex flex-col gap-2.5 font-poppins text-lg">
+      <h2>{category}</h2>
+      <div className="flex flex-col">
         {articles.map((article, id) => (
           <Link
             href={`/${article.id}`}
             key={id}
-            className="text-neutral-900 hover:text-amber-700 transition duration-150"
           >
             {article.title}
           </Link>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
@@ -6,14 +6,7 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        cormorantGaramond: ["var(--font-cormorant-garamond)", "serif"],
-        poppins: ["var(--font-poppins)", "sans-serif"],
-      },
-    },
-  },
+  theme: {},
   plugins: [],
-}
-export default config
+};
+export default config;
