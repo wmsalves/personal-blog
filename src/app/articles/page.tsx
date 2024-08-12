ea4c63434560e3b2c5fa93c40846b127a5bcdd1f
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-08-12 11:48:42 -0300

    chore: Update date format in articles page

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,11 +1,9 @@
 import { Article, getArticles } from "@/src/lib/articles";
 import moment from "moment";
-
 import Link from "next/link";
 
 export default async function ArticlesPage() {
   const articles = getArticles();
-
   return (
     <div className="w-full">
       <div className="flex items-center justify-between">
@@ -14,7 +12,7 @@ export default async function ArticlesPage() {
           <p className="underline">RSS Feed</p>
         </Link>
       </div>
-      <section className=" flex flex-col divide-y divide-zinc-300 w-full">
+      <section className="flex flex-col divide-y divide-zinc-300 w-full">
         {articles.map((article: Article) => (
           <Link
             className="flex items-center justify-between w-full py-2 hover:bg-zinc-100 px-4"
@@ -23,7 +21,7 @@ export default async function ArticlesPage() {
           >
             {article.title}{" "}
             <span className="text-zinc-500">
-              {moment(article.date, "DD-MM-YYYY").format("MMMM Do, YYYY")}
+              {moment(article.date, "YYYY-MM-DD").format("MMMM Do, YYYY")}
             </span>
           </Link>
         ))}
