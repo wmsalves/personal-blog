d401f887c829766fe6cf4aa2ccae43bde4a42e95
Luke Berry <lukeberrypi@gmail.com>
2024-11-17 15:36:13 +0000

    details

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -47,8 +47,11 @@ export default function Header() {
           className="relative flex no-underline items-center max-sm:text-center"
           href="/"
         >
-          <div className="w-12 h-full flex items-center absolute -left-9 size-5 data-[is-home=true]:hidden">
-            <ChevronLeft data-is-home={isHome} />
+          <div
+            data-is-home={isHome}
+            className="w-12 h-full flex items-center absolute -left-9 size-5 data-[is-home=true]:hidden"
+          >
+            <ChevronLeft />
           </div>
           <div className="flex w-full flex-col">
             <span className="font-bold">LukeberryPi</span>
