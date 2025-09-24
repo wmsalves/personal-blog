ae15fce474f58e0fa611d2254936b06bdb84e607
Infinity <wemersoncontas22@gmail.com>
2025-09-24 18:02:00 -0300

    feat(about): add personal about section with Spotify, education and career journey

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,83 +6,66 @@ export default function AboutPage() {
       </h1>
       <article>
         <p>
-          I was born in São Paulo and moved to{" "}
-          <a href="https://en.wikipedia.org/wiki/Florian%C3%B3polis">
-            Florianópolis
+          I was born in Contagem and moved to{" "}
+          <a href="https://en.wikipedia.org/wiki/Belo_Horizonte">
+            Belo Horizonte
           </a>{" "}
-          as a 10 year old kid. I fell in love with the nature, sport and music
-          culture of the Magical Island.
+          where I currently live. I fell in love with technology, games and the
+          culture of building things that can impact people&apos;s lives.
         </p>
         <p>
           In the next decade, I{" "}
-          <a href="https://open.spotify.com/artist/4ymHKBTThfeOL114lwCWPQ?si=c30897c35e404259">
-            became a singer-songwriter
+          <a href="https://open.spotify.com/user/veidoido?si=63342231a41f422e">
+            explored music
           </a>
-          , got a Karate blue belt, won back-to-back Carnavals with a samba
-          school and wasted thousands of hours playing League of Legends.
+          , played a lot of video games and started to get curious about how
+          software and systems really work behind the scenes.
         </p>
         <p>
-          Then I studied Economics at{" "}
-          <a href="https://en.wikipedia.org/wiki/Federal_University_of_Santa_Catarina">
-            Federal University of Santa Catarina
+          Then I studied Information Systems at{" "}
+          <a href="https://www.pucminas.br">
+            Pontifical Catholic University of Minas Gerais (PUC Minas)
           </a>
-          , mostly for my love for History and Math. I learned a lot of
-          Politics, Statistics and Macroeconomics.
+          , where I focused on backend development and expanded my knowledge in
+          databases, APIs and programming languages like TypeScript, JavaScript,
+          Node.js and C#.
         </p>
         <p>
-          But I&apos;d spent too long studying models that weren&apos;t
-          applicable in real-life. I felt disconnected from my studies which led
-          to me dropping out after 2 years.
-        </p>
-        <p>
-          Then came my first contact with programming: as a Data Analyst at a
-          Tech startup. We used Microsoft Excel and Power BI to generate our
-          reports.
-        </p>
-        <p>
-          With millions of data entries, it wasn&apos;t long until my Dell
-          notebook became unusably slow. This led me to learn Python and Pandas
-          to better deal with datasets, as well as Selenium and pyautogui.
-        </p>
-        <p>
-          I&apos;ve always been obsessed with user interfaces. My friends
-          couldn&apos;t bear me pointing out broken buttons and ugly websites
-          all the time.
-        </p>
-        <p>
-          When I found out what HTML was, I did a deep dive into the Frontend. I
-          completed FreeCodeCamp&apos;s{" "}
-          <a href="https://www.freecodecamp.org/learn/2022/responsive-web-design/">
-            Responsive Web Design
+          My curiosity led me to dive deeper into web technologies. I learned{" "}
+          <a href="https://developer.mozilla.org/en-US/docs/Web/HTML">HTML</a>,{" "}
+          <a href="https://developer.mozilla.org/en-US/docs/Web/CSS">CSS</a> and{" "}
+          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
+            JavaScript
           </a>{" "}
-          and swiftly progressed to Javascript and React soon after.
+          before progressing to frameworks like Vue.js, React and Angular,
+          always aiming to connect frontend and backend in meaningful ways.
         </p>
         <p>
-          I became more active within the Brazilian Dev community, sharing
-          quirky UI bugs and building my projects in public such as{" "}
-          <a href="https://phived.com">phived</a> and{" "}
-          <a href="https://css2wind.com">css2wind</a>. I collaborated with
-          hundreds of developers and contributed a lot to Open Source.
+          I&apos;ve been building projects that integrate APIs, databases like{" "}
+          <a href="https://www.postgresql.org/">PostgreSQL</a> and{" "}
+          <a href="https://www.microsoft.com/en-us/sql-server">SQL Server</a>,
+          and experimenting with{" "}
+          <a href="https://en.wikipedia.org/wiki/Machine_learning">
+            Machine Learning
+          </a>{" "}
+          applied to games — which also became the theme of my thesis.
         </p>
         <p>
-          In 2024, I traveled to Torino, Italy to visit my dad. My life flipped
-          upside down and I ended up living in Málaga for a while. After I{" "}
-          <a href="/articles/how-not-to-get-a-job-in-spain">
-            failed to get a job
-          </a>
-          , I moved to the UK and landed a job in London, where I currently
-          live.
+          Today, I work as a Junior Developer, where I keep improving my skills,
+          collaborating with teams, and contributing to building efficient
+          server-side solutions.
         </p>
         <p>
           If you want to get in touch, send me a DM on{" "}
-          <a href="https://x.com/lukeberrypi">X</a> or an email{" "}
-          <a href="mailto:lukeberrypi@gmail.com">lukeberrypi@gmail.com</a>.
+          <a href="https://www.linkedin.com/in/wmsalves/">LinkedIn</a> or an
+          email <a href="mailto:devwmsalves@gmail.com">devwmsalves@gmail.com</a>
+          .
         </p>
         <p>Smile, you&apos;re alive :)</p>
         <p>
           Best,
           <br />
-          Luke
+          Wemerson
         </p>
       </article>
     </>
